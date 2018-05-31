2005b4e35b4d5159177b15f72878928df475b167
Makoto <inouemak@googlemail.com>
2018-05-31 23:05:33 +0100

    Add ens and dsnregistrar into migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-var rsasha1   = artifacts.require("dnssec-oracle/contracts/RSASHA1Algorithm.sol");
-var rsasha256 = artifacts.require("dnssec-oracle/contracts/RSASHA256Algorithm.sol");
-var sha1 = artifacts.require("dnssec-oracle/contracts/SHA1Digest.sol");
-var sha256 = artifacts.require("dnssec-oracle/contracts/SHA256Digest.sol");
-var nsec3sha1 = artifacts.require("dnssec-oracle/contracts/SHA1NSEC3Digest.sol");
-var dnssec = artifacts.require("dnssec-oracle/contracts/DNSSEC.sol");
+var DNSSEC = artifacts.require("dnssec-oracle/contracts/DNSSEC.sol");
 var dummyalgorithm = artifacts.require("dnssec-oracle/contracts/DummyAlgorithm.sol");
 var dummydigest = artifacts.require("dnssec-oracle/contracts/DummyDigest.sol");
-
+var ENSImplementation = artifacts.require("dnsregistrar/contracts/ENSImplementation.sol");
+var DNSRegistrar = artifacts.require("dnsregistrar/contracts/DNSRegistrar.sol");
 var dns = require("dnssec-oracle/lib/dns.js");
+var namehash = require('eth-ens-namehash');
+var sha3= require('web3').utils.sha3;
+var tld = "test";
+let ens, algorithm, digest;
 
 function encodeAnchors(anchors) {
   var buf = new Buffer(4096);
@@ -55,24 +55,24 @@ module.exports = function(deployer, network) {
       digest: new Buffer("", "hex")
     });
   }
-  return deployer.deploy(dnssec, encodeAnchors(anchors))
-    .then(() => deployer.deploy([[rsasha256], [rsasha1], [sha256], [sha1], [nsec3sha1]]))
-    .then(() => dev?deployer.deploy([[dummyalgorithm], [dummydigest]]):null)
-    .then(() => dnssec.deployed().then(function(instance) {
+  return deployer.deploy(DNSSEC, encodeAnchors(anchors))
+    .then(() => deployer.deploy([[ENSImplementation],[dummyalgorithm], [dummydigest]]))
+    .then(() => ENSImplementation.deployed().then(_ens => ens = _ens))
+    .then(() => dummyalgorithm.deployed().then(_algorithm => algorithm = _algorithm))
+    .then(() => dummydigest.deployed().then(_digest => digest = _digest))
+    .then(() => DNSSEC.deployed().then(_dnssec => dnssec = _dnssec))
+    .then(() => deployer.deploy(DNSRegistrar, dnssec.address, ens.address, dns.hexEncodeName(tld + "."), namehash.hash(tld)))
+    .then(() => DNSRegistrar.deployed().then(_registrar => registrar = _registrar))
+    .then(() => ENSImplementation.deployed().then(_ens => _ens.setSubnodeOwner(0, sha3(tld), registrar.address)))
+    .then(() => DNSSEC.deployed().then((_dnssec) => {
       tasks = [];
-      tasks.push(rsasha1.deployed().then(async function(algorithm) {
-        await instance.setAlgorithm(5, algorithm.address);
-        await instance.setAlgorithm(7, algorithm.address);
+      tasks.push(dummyalgorithm.deployed().then(async function(algorithm) {
+        await _dnssec.setAlgorithm(253, algorithm.address);
+        await _dnssec.setAlgorithm(254, algorithm.address);
       }));
-      tasks.push(rsasha256.deployed().then((algorithm) => instance.setAlgorithm(8, algorithm.address)));
-      tasks.push(sha1.deployed().then((digest) => instance.setDigest(1, digest.address)));
-      tasks.push(sha256.deployed().then((digest) => instance.setDigest(2, digest.address)));
-      tasks.push(nsec3sha1.deployed().then((digest) => instance.setNSEC3Digest(1, digest.address)));
-      if(dev) {
-        tasks.push(dummyalgorithm.deployed().then((algorithm) => instance.setAlgorithm(253, algorithm.address)));
-        tasks.push(dummyalgorithm.deployed().then((algorithm) => instance.setAlgorithm(254, algorithm.address)));
-        tasks.push(dummydigest.deployed().then((digest) => instance.setDigest(253, digest.address)));
-      }
+      tasks.push(dummydigest.deployed().then(async function(digest) {
+        await _dnssec.setDigest(253, digest.address);
+      }));
       return Promise.all(tasks);
     }));
 };
