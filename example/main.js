12aba0596e98d0a00bfc897e554017d6137f1605
Makoto <inouemak@googlemail.com>
2018-06-06 14:38:13 +0100

    Change demo to show rrdata

--- example/main.js
+++ example/main.js
@@ -4,6 +4,16 @@ const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const MyContract = require("../build/contracts/DNSSEC.json");
 const network = Object.keys(MyContract.networks)[0];
 const address = MyContract.networks[network].address;
-
 const dnsprove  = new DnsProve(provider);
-dnsprove.lookup('_ens.matoken.xyz').then(console.log)
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("lookup-button").onclick = function (a,b){
+    let input = document.getElementById("lookup-input").value;
+    console.log('input', input)
+    document.getElementById("lookup-output").innerHTML = input;
+    dnsprove.lookup(input).then(function(r){
+      document.getElementById("lookup-output").innerHTML = r.display().map((c)=>{return c.join('\n');}).join('\n');
+    })
+    return false;
+  }
+})
