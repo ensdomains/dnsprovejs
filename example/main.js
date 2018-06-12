3f03fea7b2bb9083c23e4a8a846ce9ef65872ad7
Makoto <inouemak@googlemail.com>
2018-06-12 18:00:03 +0100

    Claim ens address via dnsregistrar

--- example/main.js
+++ example/main.js
@@ -1,10 +1,10 @@
+const namehash = require('eth-ens-namehash');
 const DnsProve  = require('./../lib/dnsprover.js');
 const DNSSEC = require("../build/contracts/DNSSEC.json");
-const contractLength = Object.keys(DNSSEC.networks).length;
-const network = Object.keys(DNSSEC.networks)[contractLength -1];
-const oracleAddress = DNSSEC.networks[network].address;
+const ENSImplementation = require("../build/contracts/ensimplementation.json");
+const DNSRegistrar = require("../build/contracts/dnsregistrar.json");
 const Web3      = require('web3');
-console.log('oracleAddress', oracleAddress);
+const dns = require("dnssec-oracle/lib/dns.js");
 
 function updateDOM(element, message, override){
   if(override){
@@ -26,6 +26,21 @@ function askOracle(){
   updateDOM('oracle-output', 'The address is owned by ' +  window.result.owner)
 }
 
+function askEns(input, cb){
+  console.log('owner', input, namehash.hash(input));
+  window.ens.owner(namehash.hash(input), (error, r)=>{
+    console.log('ens', error, r);
+    cb(r);
+  })
+}
+
+function claim(name, proof){
+  let encodedProof = '0x' + proof.rrdata.toString('hex');
+  window.dnsregistrar.claim(dns.hexEncodeName(name + '.'), encodedProof, {from:window.result.owner}, (error, r)=>{
+    console.log('claimed', r);
+  });
+}
+
 function submitProof(proofs, i){
   let proof = proofs[i]
   window.oracle.knownProof(proof).then(function(r){
@@ -35,6 +50,11 @@ function submitProof(proofs, i){
         if(i < proofs.length - 1){
           submitProof(proofs, i+1)
         }else{
+          askEns(window.input, (r)=>{
+            if(r == '0x0000000000000000000000000000000000000000'){
+              claim(window.input, proof)
+            }
+          })  
           updateDOM('oracle-output', 'Click Lookup button to check the latest state');
         }
       });
@@ -42,6 +62,11 @@ function submitProof(proofs, i){
       if(i < proofs.length - 1){
         submitProof(proofs, i+1)
       }else{
+        askEns(window.input, (r)=>{
+          if(r == '0x0000000000000000000000000000000000000000'){
+            claim(window.input, proof)
+          }
+        })
         updateDOM('oracle-output', 'Click Lookup button to check the latest state');
       }
     }
@@ -58,18 +83,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log('Using local provider')
   }
 
-  window.dnsprove  = new DnsProve(provider);  
-  window.oracle = dnsprove.getOracle(oracleAddress)  
-
+  // They are pre web3 1.0 syntax loaded via metamask
+  web3.version.getNetwork((error, network)=>{
+    const oracleAddress = DNSSEC.networks[network].address;
+    const oracleAbi =     DNSSEC.abi;
+    const ensAddress = ENSImplementation.networks[network].address;
+    const ensAbi = ENSImplementation.abi;
+    const dnsregistrarAddress = DNSRegistrar.networks[network].address;
+    const dnsregistrarAbi = DNSRegistrar.abi;
+    var OracleContract = web3.eth.contract(oracleAbi);
+    var ENSContract = web3.eth.contract(ensAbi);
+    var DNSRegistrarContract = web3.eth.contract(dnsregistrarAbi);
+    window.ens = ENSContract.at(ensAddress);
+    window.dnsregistrar = DNSRegistrarContract.at(dnsregistrarAddress);
+    window.dnsprove  = new DnsProve(provider);  
+    window.oracle = dnsprove.getOracle(oracleAddress)
+    window.oldOracle = OracleContract.at(oracleAddress);
+    window.ensEvents = ens.allEvents({fromBlock: 0, toBlock: 'latest'});
+    window.dnsregistrarEvents = dnsregistrar.allEvents({fromBlock: 0, toBlock: 'latest'});
+    window.oracleEvents = oldOracle.allEvents({fromBlock: 0, toBlock: 'latest'});
+  })
+  
   document.getElementById("lookup-button").onclick = function (){
     document.getElementById("lookup-output").innerHTML = '';
-    let input = document.getElementById("lookup-input").value;
-    document.getElementById("lookup-output").innerHTML = input;
-    dnsprove.lookup(input).then(function(r){
+    window.input = document.getElementById("lookup-input").value;
+    document.getElementById("lookup-output").innerHTML = window.input;
+    dnsprove.lookup('_ens.' + window.input).then(function(r){
       window.result = r
       document.getElementById("lookup-output").innerHTML = r.display().map((c)=>{
         return c.join('\n');
       }).join('\n');
+      askEns(window.input, (r)=>{
+        updateDOM('ens-lookup-output', r || input + ' is not found on ENS', true);
+      })
       askOracle()
     })
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -25,39 +25,9 @@ function encodeAnchors(anchors) {
 
 module.exports = function(deployer, network) {
   var test = (network == "test");
-  var anchors = [
-    {
-      name: ".",
-      type: dns.TYPE_DS,
-      klass: dns.CLASS_INET,
-      ttl: 3600,
-      keytag: 19036,
-      algorithm: 8,
-      digestType: 2,
-      digest: new Buffer("49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5", "hex")
-    },
-    {
-      name: ".",
-      type: dns.TYPE_DS,
-      klass: dns.CLASS_INET,
-      ttl: 3600,
-      keytag: 20326,
-      algorithm: 8,
-      digestType: 2,
-      digest: new Buffer("E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D", "hex")
-    },
-  ];
+  var anchors = dns.anchors;
   if(test) {
-    anchors.push({
-      name: ".",
-      type: dns.TYPE_DS,
-      klass: dns.CLASS_INET,
-      ttl: 3600,
-      keytag: 5647, // Empty body, flags == 0x0101, algorithm = 253, body = 0x1111
-      algorithm: 253,
-      digestType: 253,
-      digest: new Buffer("", "hex")
-    });
+    anchors.push(dns.dummyAnchor);
   }
 
   return deployer.deploy(DNSSEC, encodeAnchors(anchors))
