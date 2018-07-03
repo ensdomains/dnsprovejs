7a52c1c3d7143c7f099ab71cefe063adfbcdc146
Makoto <inouemak@googlemail.com>
2018-07-03 11:56:53 +0100

    Add pretier

--- example/main.js
+++ example/main.js
@@ -1,97 +1,110 @@
 const namehash = require('eth-ens-namehash');
-const DnsProve  = require('./../lib/dnsprover.js');
-const DNSSEC = require("../build/contracts/DNSSEC.json");
-const ENSImplementation = require("../build/contracts/ensimplementation.json");
-const DNSRegistrar = require("../build/contracts/dnsregistrar.json");
-const Web3      = require('web3');
+const DnsProve = require('./../lib/dnsprover.js');
+const DNSSEC = require('../build/contracts/DNSSEC.json');
+const ENSImplementation = require('../build/contracts/ensimplementation.json');
+const DNSRegistrar = require('../build/contracts/dnsregistrar.json');
+const Web3 = require('web3');
 const packet = require('dns-packet');
 
-function hexEncodeName(name){
+function hexEncodeName(name) {
   return '0x' + packet.name.encode(name).toString('hex');
 }
 
-function updateDOM(element, message, override){
-  if(override){
+function updateDOM(element, message, override) {
+  if (override) {
     document.getElementById(element).innerHTML = message;
-  }else{
-    document.getElementById(element).innerHTML = document.getElementById(element).innerHTML   + '\n' + message;
+  } else {
+    document.getElementById(element).innerHTML =
+      document.getElementById(element).innerHTML + '\n' + message;
   }
 }
 
-function askOracle(){
-  document.getElementById("oracle-output").innerHTML = '';
+function askOracle() {
+  document.getElementById('oracle-output').innerHTML = '';
   let proofs = window.result.proofs;
-  proofs.forEach(function(proof){
+  proofs.forEach(function(proof) {
     let rrdata;
-    window.oracle.knownProof(proof).then(function(r){
-      updateDOM('oracle-output', proof.name + '\t' + proof.type + '\t' + r)
+    window.oracle.knownProof(proof).then(function(r) {
+      updateDOM('oracle-output', proof.name + '\t' + proof.type + '\t' + r);
     });
-  })
-  let owner = window.result.results[5].rrs[0].data.toString().split('=')[1], owner);
-  updateDOM('oracle-output', 'The address is owned by ' +  owner)
+  });
+  let lastResult = window.result.results.length - 1;
+  let owner = lastResult.rrs[0].data.toString().split('=')[1];
+  updateDOM('oracle-output', 'The address is owned by ' + owner);
 }
 
-function askEns(input, cb){
-  console.log('owner', input, namehash.hash(input));
-  window.ens.owner(namehash.hash(input), (error, r)=>{
-    console.log('ens', error, r);
+function askEns(input, cb) {
+  window.ens.owner(namehash.hash(input), (error, r) => {
     cb(r);
-  })
-}
-
-function claim(name, proof){
-  let encodedProof = '0x' + proof.rrdata.toString('hex');
-  window.dnsregistrar.claim(hexEncodeName(name + '.'), encodedProof, {from: web3.eth.defaultAccount}, (error, r)=>{
-    console.log('claimed', r);
   });
 }
 
-function submitProof(proofs, i){
-  let proof = proofs[i]
-  window.oracle.knownProof(proof).then(function(r){
-    if(r == '0x0000000000000000000000000000000000000000'){
-      window.oracle.submitProof(proof, proofs[i-1], {from: web3.eth.defaultAccount}).then(function(r){
-        console.log('result', i, r);
-        if(i < proofs.length - 1){
-          submitProof(proofs, i+1)
-        }else{
-          askEns(window.input, (r)=>{
-            if(r == '0x0000000000000000000000000000000000000000'){
-              claim(window.input, proof)
-            }
-          })  
-          updateDOM('oracle-output', 'Click Lookup button to check the latest state');
-        }
-      });
-    }else{
-      if(i < proofs.length - 1){
-        submitProof(proofs, i+1)
-      }else{
-        askEns(window.input, (r)=>{
-          if(r == '0x0000000000000000000000000000000000000000'){
-            claim(window.input, proof)
+function claim(name, proof) {
+  let encodedProof = '0x' + proof.rrdata.toString('hex');
+  window.dnsregistrar.claim(
+    hexEncodeName(name + '.'),
+    encodedProof,
+    { from: web3.eth.defaultAccount },
+    (error, r) => {
+      console.log('claimed', r);
+    }
+  );
+}
+
+function submitProof(proofs, i) {
+  let proof = proofs[i];
+  window.oracle.knownProof(proof).then(function(r) {
+    if (r == '0x0000000000000000000000000000000000000000') {
+      window.oracle
+        .submitProof(proof, proofs[i - 1], { from: web3.eth.defaultAccount })
+        .then(function(r) {
+          console.log('result', i, r);
+          if (i < proofs.length - 1) {
+            submitProof(proofs, i + 1);
+          } else {
+            askEns(window.input, r => {
+              if (r == '0x0000000000000000000000000000000000000000') {
+                claim(window.input, proof);
+              }
+            });
+            updateDOM(
+              'oracle-output',
+              'Click Lookup button to check the latest state'
+            );
           }
-        })
-        updateDOM('oracle-output', 'Click Lookup button to check the latest state');
+        });
+    } else {
+      if (i < proofs.length - 1) {
+        submitProof(proofs, i + 1);
+      } else {
+        askEns(window.input, r => {
+          if (r == '0x0000000000000000000000000000000000000000') {
+            claim(window.input, proof);
+          }
+        });
+        updateDOM(
+          'oracle-output',
+          'Click Lookup button to check the latest state'
+        );
       }
     }
   });
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   if (typeof web3 !== 'undefined') {
     // Use the browser's ethereum provider
-    var provider = web3.currentProvider
-    console.log('Using metamask')
+    var provider = web3.currentProvider;
+    console.log('Using metamask');
   } else {
-    var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-    console.log('Using local provider')
+    var provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    console.log('Using local provider');
   }
 
   // They are pre web3 1.0 syntax loaded via metamask
-  web3.version.getNetwork((error, network)=>{
+  web3.version.getNetwork((error, network) => {
     const oracleAddress = DNSSEC.networks[network].address;
-    const oracleAbi =     DNSSEC.abi;
+    const oracleAbi = DNSSEC.abi;
     const ensAddress = ENSImplementation.networks[network].address;
     const ensAbi = ENSImplementation.abi;
     const dnsregistrarAddress = DNSRegistrar.networks[network].address;
@@ -101,39 +114,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var DNSRegistrarContract = web3.eth.contract(dnsregistrarAbi);
     window.ens = ENSContract.at(ensAddress);
     window.dnsregistrar = DNSRegistrarContract.at(dnsregistrarAddress);
-    window.dnsprove  = new DnsProve(provider);  
-    window.oracle = dnsprove.getOracle(oracleAddress)
+    window.dnsprove = new DnsProve(provider);
+    window.oracle = dnsprove.getOracle(oracleAddress);
     window.oldOracle = OracleContract.at(oracleAddress);
-    window.ensEvents = ens.allEvents({fromBlock: 0, toBlock: 'latest'});
-    window.dnsregistrarEvents = dnsregistrar.allEvents({fromBlock: 0, toBlock: 'latest'});
-    window.oracleEvents = oldOracle.allEvents({fromBlock: 0, toBlock: 'latest'});
-  })
-  
-  document.getElementById("lookup-button").onclick = function (){
-    document.getElementById("lookup-output").innerHTML = '';
-    window.input = document.getElementById("lookup-input").value;
-    document.getElementById("lookup-output").innerHTML = window.input;
-    dnsprove.lookup('TXT', '_ens.' + window.input).then(function(r){
-      window.result = r
-      if(result.found){
-        document.getElementById("lookup-output").innerHTML = r.display().map((c)=>{
-          return c.join('\n');
-        }).join('\n');
-        askEns(window.input, (r)=>{
-          updateDOM('ens-lookup-output', r || input + ' is not found on ENS', true);
-        })
-        askOracle()
-      }else{
-        document.getElementById("lookup-output").innerHTML = 'the entry does not exist on DNS';
+    window.ensEvents = ens.allEvents({ fromBlock: 0, toBlock: 'latest' });
+    window.dnsregistrarEvents = dnsregistrar.allEvents({
+      fromBlock: 0,
+      toBlock: 'latest'
+    });
+    window.oracleEvents = oldOracle.allEvents({
+      fromBlock: 0,
+      toBlock: 'latest'
+    });
+  });
+
+  document.getElementById('lookup-button').onclick = function() {
+    document.getElementById('lookup-output').innerHTML = '';
+    window.input = document.getElementById('lookup-input').value;
+    document.getElementById('lookup-output').innerHTML = window.input;
+    dnsprove.lookup('TXT', '_ens.' + window.input).then(function(r) {
+      window.result = r;
+      if (result.found) {
+        document.getElementById('lookup-output').innerHTML = r
+          .display()
+          .map(c => {
+            return c.join('\n');
+          })
+          .join('\n');
+        askEns(window.input, r => {
+          updateDOM(
+            'ens-lookup-output',
+            r || input + ' is not found on ENS',
+            true
+          );
+        });
+        askOracle();
+      } else {
+        document.getElementById('lookup-output').innerHTML =
+          'the entry does not exist on DNS';
       }
-    })
-  }
-  document.getElementById("submit-button").onclick = function (){
+    });
+  };
+  document.getElementById('submit-button').onclick = function() {
     updateDOM('oracle-output', '', true);
-    if(!window.result){
+    if (!window.result) {
       updateDOM('oracle-output', 'Please lookup DNS first');
       return false;
     }
-    submitProof(window.result.proofs, 0)
-  }
-})
+    submitProof(window.result.proofs, 0);
+  };
+});
--- test/integration.js
+++ test/integration.js
@@ -1,18 +1,26 @@
 const sinon = require('sinon');
 const fs = require('fs');
 const packet = require('dns-packet');
-const DnsProve  = require('../lib/dnsprover');
+const DnsProve = require('../lib/dnsprover');
 const namehash = require('eth-ens-namehash');
-const sha3= require('web3').utils.sha3;
-const dns = require("@ensdomains/dnssec-oracle/lib/dns.js");
-const DNSSEC = artifacts.require("@ensdomains/dnssec-oracle/DNSSEC.sol");
-const DummyAlgorithm = artifacts.require("@ensdomains/dnssec-oracle/DummyAlgorithm.sol");
-const DummyDigest = artifacts.require("@ensdomains/dnssec-oracle/DummyDigest.sol");
-const ENSImplementation = artifacts.require("@ensdomains/dnsregistrar/ENSImplementation.sol");
-const DNSRegistrar = artifacts.require("@ensdomains/dnsregistrar/DNSRegistrar.sol");
+const sha3 = require('web3').utils.sha3;
+const dns = require('@ensdomains/dnssec-oracle/lib/dns.js');
+const DNSSEC = artifacts.require('@ensdomains/dnssec-oracle/DNSSEC.sol');
+const DummyAlgorithm = artifacts.require(
+  '@ensdomains/dnssec-oracle/DummyAlgorithm.sol'
+);
+const DummyDigest = artifacts.require(
+  '@ensdomains/dnssec-oracle/DummyDigest.sol'
+);
+const ENSImplementation = artifacts.require(
+  '@ensdomains/dnsregistrar/ENSImplementation.sol'
+);
+const DNSRegistrar = artifacts.require(
+  '@ensdomains/dnsregistrar/DNSRegistrar.sol'
+);
 const tld = 'xyz';
 
-function hexEncodeName(name){
+function hexEncodeName(name) {
   return '0x' + packet.name.encode(name).toString('hex');
 }
 
@@ -20,17 +28,17 @@ contract('DNSSEC', function(accounts) {
   const owner = accounts[0];
   const nonOwner = accounts[1];
   const provider = web3.currentProvider;
-  
+
   let stub = sinon.stub(global, 'fetch').callsFake(function(input) {
     return {
-      buffer: async ()=>{
+      buffer: async () => {
         // eg: AAEBAAABAAAAAAABBF9lbnMHbWF0b2tlbgN4eXoAABAAAQAAKRAAAACAAAAA
         let fileName = input.split('=')[2];
         let filePath = './test/fixtures/' + fileName + '.json';
         let response;
         if (fs.existsSync(filePath)) {
           response = fs.readFileSync(filePath, 'utf8');
-        }else{
+        } else {
           response = fs.readFileSync('./test/fixtures/notfound.json', 'utf8');
         }
         let decoded = JSON.parse(response, (k, v) => {
@@ -39,18 +47,23 @@ contract('DNSSEC', function(accounts) {
           // You need to transform back to Buffer
           // such as {data: new Buffer([1,2,1]) }
           if (
-            v !== null            &&
-            typeof v === 'object' && 
-            'type' in v           &&
-            v.type === 'Buffer'   &&
-            'data' in v           &&
-            Array.isArray(v.data)) {
-              v = new Buffer(v.data);
+            v !== null &&
+            typeof v === 'object' &&
+            'type' in v &&
+            v.type === 'Buffer' &&
+            'data' in v &&
+            Array.isArray(v.data)
+          ) {
+            v = new Buffer(v.data);
           }
           return v;
         });
         // Swap address with dnsregistrar owner as only the address owner can register
-        if (decoded.answers.length > 0 && decoded.answers[0].name == '_ens.matoken.xyz' && decoded.answers[0].type == 'TXT'){
+        if (
+          decoded.answers.length > 0 &&
+          decoded.answers[0].name == '_ens.matoken.xyz' &&
+          decoded.answers[0].type == 'TXT'
+        ) {
           let text = `a=${owner}`;
           decoded.answers[0].data = Buffer.from(text, 'ascii');
         }
@@ -60,17 +73,22 @@ contract('DNSSEC', function(accounts) {
   });
 
   let address, ens, dummyAlgorithm, dummyDigest, registrar, dnssec;
-  beforeEach(async function(){
+  beforeEach(async function() {
     ens = await ENSImplementation.new();
     dummyAlgorithm = await DummyAlgorithm.new();
     dummyDigest = await DummyDigest.new();
     let anchors = dns.anchors;
-    anchors.push(dns.dummyAnchor)
+    anchors.push(dns.dummyAnchor);
     dnssec = await DNSSEC.new(dns.encodeAnchors(anchors));
-    address =  dnssec.address;
+    address = dnssec.address;
 
     assert.equal(await dnssec.anchors.call(), dns.encodeAnchors(anchors));
-    registrar = await DNSRegistrar.new(dnssec.address, ens.address, hexEncodeName(tld), namehash.hash(tld));
+    registrar = await DNSRegistrar.new(
+      dnssec.address,
+      ens.address,
+      hexEncodeName(tld),
+      namehash.hash(tld)
+    );
 
     assert.equal(await registrar.oracle.call(), dnssec.address);
     assert.equal(await registrar.ens.call(), ens.address);
@@ -87,61 +105,67 @@ contract('DNSSEC', function(accounts) {
     assert.equal(await dnssec.algorithms.call(253), dummyAlgorithm.address);
     assert.equal(await dnssec.algorithms.call(254), dummyAlgorithm.address);
     assert.equal(await dnssec.digests.call(253), dummyDigest.address);
-  })
+  });
 
   it('lookup should accept mocked DNSSEC records', async function() {
     // Step 1. Look up dns entry
-    const dnsprove  = new DnsProve(provider);
+    const dnsprove = new DnsProve(provider);
     const dnsResult = await dnsprove.lookup('TXT', '_ens.matoken.xyz');
-    const oracle    = await dnsprove.getOracle(address);
+    const oracle = await dnsprove.getOracle(address);
     // Step 2. Checks that the result is found and is valid.
     assert.equal(dnsResult.found, true);
-    assert.equal(dnsResult.results[5].rrs[0].data.toString().split('=')[1], owner);
+    assert.equal(
+      dnsResult.results[5].rrs[0].data.toString().split('=')[1],
+      owner
+    );
     assert.equal(dnsResult.proofs.length, 6);
     assert.equal(dnsResult.proofs[0].name, '.');
     // Step 3. Submit each proof to DNSSEC oracle
     let proofs = dnsResult.proofs;
-    for(let i = 0; i < proofs.length; i++){
+    for (let i = 0; i < proofs.length; i++) {
       var proof = proofs[i];
       let rrdata;
       let result = await oracle.knownProof(proof);
       assert.equal(parseInt(result), 0);
-      await oracle.submitProof(proof, proofs[i-1], {from:owner})
+      await oracle.submitProof(proof, proofs[i - 1], { from: owner });
       result = await oracle.knownProof(proof);
       assert.notEqual(parseInt(result), 0);
     }
     // Step 4. Use the last rrdata as a proof to claim the ownership
-    var proof =  '0x' + proofs[proofs.length -1].rrdata.toString('hex');
-    let name = hexEncodeName("matoken.xyz");
-    let tx = await registrar.claim(name, proof, {from:owner});
+    var proof = '0x' + proofs[proofs.length - 1].rrdata.toString('hex');
+    let name = hexEncodeName('matoken.xyz');
+    let tx = await registrar.claim(name, proof, { from: owner });
     assert.equal(parseInt(tx.receipt.status), 1);
     // Step 5. Confirm that the domain is owned by thw DNS record owner.
-    let result = await ens.owner.call(namehash.hash("matoken.xyz"));
+    let result = await ens.owner.call(namehash.hash('matoken.xyz'));
     assert.equal(result, owner);
   });
 
   it('prove should return transactions which need proving', async function() {
-    const dnsprove  = new DnsProve(provider);
+    const dnsprove = new DnsProve(provider);
     let dnsResult = await dnsprove.lookup('TXT', '_ens.matoken.xyz', address);
-    let oracle    = await dnsprove.getOracle(address);
-    let prover    = await oracle.getProver(dnsResult);
+    let oracle = await dnsprove.getOracle(address);
+    let prover = await oracle.getProver(dnsResult);
     assert.equal(prover.total, 6);
     assert.equal(prover.unproven, 6);
-    assert.equal(prover.lastProof, '0x' + prover.proofs[5].rrdata.toString('hex'));
-    await oracle.submitProof(prover.proofs[0], null, {from:nonOwner});
-    prover    = await oracle.getProver(dnsResult);
-    assert.equal(prover.total, 6)
-    assert.equal(prover.unproven, 5)
+    assert.equal(
+      prover.lastProof,
+      '0x' + prover.proofs[5].rrdata.toString('hex')
+    );
+    await oracle.submitProof(prover.proofs[0], null, { from: nonOwner });
+    prover = await oracle.getProver(dnsResult);
+    assert.equal(prover.total, 6);
+    assert.equal(prover.unproven, 5);
 
-    await oracle.submit(dnsResult, {from:nonOwner});
-    prover    = await oracle.getProver(dnsResult);
-    assert.equal(prover.total, 6)
-    assert.equal(prover.unproven, 0)
+    await oracle.submit(dnsResult, { from: nonOwner });
+    prover = await oracle.getProver(dnsResult);
+    assert.equal(prover.total, 6);
+    assert.equal(prover.unproven, 0);
   });
 
   it('raises error if the DNS entry does not exist', async function() {
-    const dnsprove  = new DnsProve(provider);
+    const dnsprove = new DnsProve(provider);
     let dnsResult = await dnsprove.lookup('TXT', 'example.com', address);
     assert.equal(dnsResult.found, false);
-  })
+  });
 });
